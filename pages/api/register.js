722d830db229b54e399a88a27cd058a00de41394
BlackBird <thanadol2005@gmail.com>
2021-10-03 21:55:52 +0700

    update:readme

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
-  res.status(200).json({ user: "Ada Lovelace" });
-}
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+  console.log(body);
+  res.status(200).json({ status: "OK" });
+};
